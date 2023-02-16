fdbbd476f97a7a58905438d2c5996add6dc63bd1
Mikel <mxagar@gmail.com>
2023-02-16 10:36:24 +0100

    example: demo1

---
+++ 04_Monitoring/lab/L2_Retraining_Redeployment/demo1/demo1.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+
+# Define local directories to look in 
+directories=['/udacity1/','/udacity2/']
+# Instantiate empty dataframe: PE ratio, Stock price
+final_df = pd.DataFrame(columns=['peratio','price'])
+
+for directory in directories:
+    # Files in directory
+    filenames = os.listdir(os.getcwd()+directory)
+    for each_filename in filenames:
+        current_df = pd.read_csv(os.getcwd()+directory+each_filename)
+        # Append dataframe + reset index!
+        final_df = final_df.append(current_df).reset_index(drop=True)
+
+# Now, we could do some cleaning...
+
+# Persist aggregated dataframe
+final_df.to_csv('demo_20210330.csv')
